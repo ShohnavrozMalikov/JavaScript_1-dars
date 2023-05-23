e5b36aea118bf74839674c3c095074b1fcff9dd0
ShohnavrozMalikov <133765496+ShohnavrozMalikov@users.noreply.github.com>
2023-05-23 22:19:35 +0500

    Bu JavaScript kodlari
    Yuklandi

---
+++ js/main.js
@@ -0,0 +1,61 @@
+/*console.log("Men faqat console da ko'rinaman");*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*document.write("Men bodiyda ko'rinaman!")*/
+/*let x=2;*/
+/*let y=5;*/
+/*x=10;*/
+
+/*let x=2,y=5,z=10;*/
+
+/*let color ="#f1f1f1"*/
+
+/*let salom ="Assalomu aleykum!"*/
+/*document.write(salom);*/
+
+/* data-types:number,string*/
+
+/*let raqam=5;*/
+/*let soz="JavaScript";*/
+
+/*document.write( typeof soz);*/
+
+
+
+/* JS arifmetik hisoblashlar*/
+
+
+let x=5;
+/*x+=5; */ /*x=5+5 */
+/*x-=2;*/ /* x=-5-2 */
+/*x*=5;*/ /* x=5*5 */
+/*let qoldiq =x%4;*/       /* qoldiq chiqarish */
+/*let daraja =x**5;*/     /* kvadratini chiqarish*/
+/*++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x+"5");*/
+
+/* if=> Agar  */
+/* elce = u holda */
+
+/*if ("Salom" == "Salom"){
+    console.log("Ular teng!");
+}
+else {
+    console.log("Ular teng emas!");
+}*/
+
+/* if => agar */
+/* else if => yoki bo'lmasa */
+/* else => u holda*/
+
+if (45 == 46){
+    console.log("Bu son 45ga teng!");
+}
+else if (45 == 44){
+    console.log("Ikkala son ham 45ga teng emas!");
+}
+else{
+    console.log("Ikkala son ham 45ga teng emas!");
+}
